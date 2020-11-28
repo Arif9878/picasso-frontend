55503269a4ca8f7e755f0bea5ef61a70c6113c9b
Arif <arifboyz16@gmail.com>
2020-11-28 10:30:35 +0700

    change layout constrained

--- src/layout/core/View.vue
+++ src/layout/core/View.vue
@@ -1,11 +1,33 @@
 <template>
-  <v-main>
-    <router-view />
+  <v-main class="grey lighten-3 px-4">
+    <v-row>
+      <v-col cols="2">
+        <v-sheet rounded="lg">
+          <dashboard-core-drawer />
+        </v-sheet>
+      </v-col>
+
+      <v-col>
+        <v-sheet
+          min-height="80vh"
+          rounded="lg"
+        >
+          <router-view />
+        </v-sheet>
+      </v-col>
+    </v-row>
   </v-main>
+  <!-- <v-main>
+  </v-main> -->
 </template>
 
 <script>
   export default {
     name: 'DashboardCoreView',
+
+    components: {
+      DashboardCoreDrawer: () => import('./Drawer'),
+    },
+
   }
 </script>
